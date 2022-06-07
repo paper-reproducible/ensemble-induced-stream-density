Fix rotation

Common/_ball.py
@@ -9,7 +9,7 @@ import numpy as np
 #     return X
 
 
-def random_rot(dim, dtype, xp=np):
+def random_rot(dim, dtype):
     """Return a random rotation matrix, drawn from the Haar distribution
     (the only uniform distribution on SO(n)).
     The algorithm is described in the paper
@@ -18,17 +18,17 @@ def random_rot(dim, dtype, xp=np):
     on Numerical Analysis, 17(3), pp. 403-409, 1980.
     For more information see
     http://en.wikipedia.org/wiki/Orthogonal_matrix#Randomization"""
-    H = xp.eye(dim, dtype=dtype)
-    D = xp.ones((dim,), dtype=dtype)
+    H = np.eye(dim, dtype=dtype)
+    D = np.ones((dim,), dtype=dtype)
     for n in range(1, dim):
-        x = xp.random.normal(size=(dim - n + 1,)).astype(dtype)
-        D[n - 1] = xp.sign(x[0])
-        x[0] -= D[n - 1] * xp.sqrt((x * x).sum())
+        x = np.random.standard_normal(size=(dim - n + 1,)).astype(dtype)
+        D[n - 1] = np.sign(x[0])
+        x[0] -= D[n - 1] * np.sqrt((x * x).sum())
         # Householder transformation
-        Hx = xp.eye(dim - n + 1, dtype=dtype) - 2.0 * xp.outer(x, x) / (x * x).sum()
-        mat = xp.eye(dim, dtype=dtype)
+        Hx = np.eye(dim - n + 1, dtype=dtype) - 2.0 * np.outer(x, x) / (x * x).sum()
+        mat = np.eye(dim, dtype=dtype)
         mat[n - 1 :, n - 1 :] = Hx
-        H = xp.dot(H, mat)
+        H = np.dot(H, mat)
     # Fix the last sign such that the determinant is 1
     D[-1] = (-1) ** (1 - dim % 2) * D.prod()
     # Equivalent to mult(numx.diag(D), H) but faster
@@ -37,11 +37,11 @@ def random_rot(dim, dtype, xp=np):
 
 
 def rotate(X, SO=None):
-    xp, _ = get_array_module(X)
+    xp, xpUtils = get_array_module(X)
     dims = X.shape[1]
 
     if SO is None:
-        SO = random_rot(dims, dtype=X.dtype, xp=xp)
+        SO = random_rot(dims, dtype=xpUtils.numpy_dtype(X))
 
     return xp.matmul(X, SO), SO
 