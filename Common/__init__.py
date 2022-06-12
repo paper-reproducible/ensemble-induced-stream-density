a80da24f3ee5d1625b8f08910b1ce5e361721153
Barry Qin <xiaoyu.qin@ieee.org>
2022-06-11 20:51:20 -0700

    Learning curve is still on its way...

--- Common/__init__.py
+++ Common/__init__.py
@@ -1,12 +1,13 @@
-from ._utils import set_printoptions
+from ._utils import set_printoptions, func2obj
 from ._xp_utils import get_array_module, get_array_module_with_utils
 from ._sampling import ReservoirSamplingEstimator
 from ._ball import rotate, ball_scale, ball_samples
 
 __all__ = [
+    "set_printoptions",
+    "func2obj",
     "get_array_module",
     "get_array_module_with_utils",
-    "set_printoptions",
     "ReservoirSamplingEstimator",
     "rotate",
     "ball_scale",
--- Common/_utils.py
+++ Common/_utils.py
@@ -3,3 +3,25 @@ import numpy as np
 
 def set_printoptions():
     np.set_printoptions(formatter={"float_kind": "{:.4f}".format})
+
+
+def func2obj(className, methodName, sample_func, **kwargs):
+    varList = sample_func.__code__.co_varnames
+    memberDict = {}
+
+    def constructur(self):
+        return
+
+    memberDict["__init__"] = constructur
+    for varName in varList:
+        memberDict[varName] = kwargs[varName]
+
+    def sample(self):
+        params = []
+        for varName in varList:
+            params = params + [getattr(self, varName)]
+        return sample_func(*params)
+
+    memberDict[methodName] = sample
+    distClass = type(className, (object,), memberDict)
+    return distClass()
